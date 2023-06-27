01f9a45f24235cc162153e646173c75277dd9cba
atsunsetree <atsunsetree529@gmail.com>
2023-06-27 20:31:13 +0900

    Fix: merge conflict

---
+++ src/main/java/org/fastcampus/util/Console.java
@@ -0,0 +1,16 @@
+package org.fastcampus.util;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        System.out.println("어떤 기능을 요청하시겠습니까?");
+
+        // 선수등록?teamId=1&name=이대호&position=1루수
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        //main메서드 확인
+
+    }
+}
